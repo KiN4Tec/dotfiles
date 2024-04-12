bfd03f2efbb1ed96c0adc46230cd6ac282ac3429
Mouhammed Nashat Helmi <nashathelmi@gmail.com>
2024-04-12 15:44:43 +0300

    Add repeat-until-success function for fish

---
+++ dot_config/private_fish/functions/repeat-until-success.fish
@@ -0,0 +1,10 @@
+function repeat-until-success
+    while true;
+        command $argv;
+        if [ $status -eq 0 ];
+            return 0;
+        end;
+        echo "Retrying in 5 seconds...";
+        sleep 5;
+    end;
+end
