f181dd2a07e4ab70dacfb2a2e00892fd13eb154e
Mouhammed Nashat Helmi <nashathelmi@gmail.com>
2024-09-14 17:40:54 +0300

    Recolorise Octave and Bash prompts

--- dot_config/bash/prompt.bash
+++ dot_config/bash/prompt.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PS1='\n\[\e[32m\]\u\[\e[0m\]@\[\e[94m\]\h\[\e[0m\] in \[\e[36;1m\]\w\n\n\[\e[32m\]\s\[\e[0m\] \[\e[36;1m\]>\[\e[0m\] '
+PS1='\n\[\e[32m\]\u\[\e[0m\]@\[\e[34m\]\h\[\e[0m\] in \[\e[36;1m\]\w\[\e[0m\]\n\n\[\e[32;1m\]\s\[\e[0m\] \[\e[36;1m\]>\[\e[0m\] '
 
 # Use the Starship prompt (if available)
 [ -n "$(command -v starship)" ] && eval "$(starship init bash)"
