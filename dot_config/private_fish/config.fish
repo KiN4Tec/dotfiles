da14a2a86b3318007e39090d959a291beb7ee84f
Mouhammed Nashat Helmi <nashathelmi@gmail.com>
2024-09-15 23:54:14 +0300

    Reorder cargo bin PATH in Fish
    Put ~/.cargo/bin in the front to override the other binaries

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,19 +1,18 @@
 # Initialization
 
-if test -d "$HOME/.cargo/bin"
-    fish_add_path -aP "$HOME/.cargo/bin"
-end
-
 if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+end
+
+if test -d "$HOME/.cargo/bin"
+    fish_add_path -pP "$HOME/.cargo/bin"
 end
 
 # Startup
 
 if status is-interactive
-   and command -sq starship
+    and command -sq starship
     starship init fish | source
 end
 
 set -g fish_greeting "Welcome back commander"
-
