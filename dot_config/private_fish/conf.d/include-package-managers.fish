a631b0a9d672d7587b6c7fb910c7d394d84d089b
Mouhammed Nashat Helmi <KiN4Tec@Gmail.com>
2026-03-04 15:36:40 +0300

    Move fish package managers init into separate file

---
+++ dot_config/private_fish/conf.d/include-package-managers.fish
@@ -0,0 +1,29 @@
+# Cargo
+if test -d "$HOME/.cargo/bin"
+    fish_add_path --append --path "$HOME/.cargo/bin"
+end
+
+# Homebrew
+if test -x "/home/linuxbrew/.linuxbrew/bin/brew"
+    set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+    set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+    set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+    fish_add_path --global --append --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
+    if test -n "$MANPATH[1]"
+        set --global --export MANPATH $MANPATH ''
+    end
+    if not contains "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
+        set --global --export INFOPATH $INFOPATH "/home/linuxbrew/.linuxbrew/share/info"
+    end
+end
+
+# NPM
+set --global --export NPM_CONFIG_PREFIX $HOME/.local/share/npm
+if test -d "$NPM_CONFIG_PREFIX"
+    fish_add_path --append --path "$NPM_CONFIG_PREFIX"
+end
+
+# Yarn
+if command -sq yarn
+    fish_add_path --append --path (yarn global bin)
+end
